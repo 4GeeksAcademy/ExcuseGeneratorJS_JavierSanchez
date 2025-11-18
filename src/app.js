ae726f58059f30f03426fc87bd6e2cbb415a979b
JSF <108297930+ZrroDev@users.noreply.github.com>
2025-11-18 20:50:56 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import "bootstrap";
-import "./style.css";
+function ExcuseGenerator(who, action, what, when) {
+  return who[Math.floor(Math.random() * who.length)] + " " + action[Math.floor(Math.random() * action.length)] 
+  + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)] + ".";
+}
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const h1 = document.getElementById("excuse");
+h1.textContent = ExcuseGenerator(who, action, what, when);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(ExcuseGenerator(who, action, what, when));
